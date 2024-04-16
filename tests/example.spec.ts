2a928e584367993ea7d95caccd4e82be158d3f0a
temagi <tema.gizz@gmail.com>
2024-04-16 14:26:05 +0200

    add example of failed and passed tests

---
+++ tests/example.spec.ts
@@ -0,0 +1,20 @@
+import { test } from '@playwright/test';
+
+// push this to github ticket
+test.describe('DOB field', () => {
+    test.only('DOB field with fill method', async ({ page }) => {
+        await page.goto('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_input_type_date');
+        await page.click('#accept-choices')
+        await page.waitForSelector('#iframeResult');
+        await page.frameLocator('#iframeResult').locator('[type=date]').fill('01011990');
+        await page.frameLocator('#iframeResult').locator('input[type="submit"]').click();
+    });
+
+    test.only('DOB field with pressSequentially method', async ({ page }) => {
+        await page.goto('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_input_type_date');
+        await page.click('#accept-choices')
+        await page.waitForSelector('#iframeResult');
+        await page.frameLocator('#iframeResult').locator('[type=date]').pressSequentially('01011990');
+        await page.frameLocator('#iframeResult').locator('input[type="submit"]').click();
+    });
+});
