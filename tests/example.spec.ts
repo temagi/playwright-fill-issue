1b6118fe2dbc25857cbb0a31009fb53bd3531bd5
temagi <tema.gizz@gmail.com>
2024-04-26 09:11:48 +0200

    add assertions

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 // push this to github ticket
 test.describe('DOB field', () => {
@@ -8,13 +8,27 @@ test.describe('DOB field', () => {
         await page.waitForSelector('#iframeResult');
         await page.frameLocator('#iframeResult').locator('[type=date]').fill('01011990');
         await page.frameLocator('#iframeResult').locator('input[type="submit"]').click();
+        await expect(page.frameLocator('#iframeResult').locator('.w3-container.w3-large.w3-border')).toBeVisible();
+        await expect(page.frameLocator('#iframeResult').locator('.w3-container.w3-large.w3-border')).toContainText('birthday=1990-01-01 ');
     });
 
-    test('Date field with pressSequentially method', async ({ page }) => {
+    test.only('Date field with pressSequentially method', async ({ page }) => {
         await page.goto('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_input_type_date');
         await page.click('#accept-choices')
         await page.waitForSelector('#iframeResult');
         await page.frameLocator('#iframeResult').locator('[type=date]').pressSequentially('01011990');
         await page.frameLocator('#iframeResult').locator('input[type="submit"]').click();
+        await expect(page.frameLocator('#iframeResult').locator('.w3-container.w3-large.w3-border')).toBeVisible();
+        await expect(page.frameLocator('#iframeResult').locator('.w3-container.w3-large.w3-border')).toContainText('birthday=1990-01-01 ');
+    });
+
+    test('Date field with type method', async ({ page }) => {
+        await page.goto('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_input_type_date');
+        await page.click('#accept-choices')
+        await page.waitForSelector('#iframeResult');
+        await page.frameLocator('#iframeResult').locator('[type=date]').type('01011990');
+        await page.frameLocator('#iframeResult').locator('input[type="submit"]').click();
+        await expect(page.frameLocator('#iframeResult').locator('.w3-container.w3-large.w3-border')).toBeVisible();
+        await expect(page.frameLocator('#iframeResult').locator('.w3-container.w3-large.w3-border')).toContainText('birthday=1990-01-01 ');
     });
 });
