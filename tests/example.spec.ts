345d7876b795fb36b2fb7f6e7a9fdca4bdb58c15
temagi <tema.gizz@gmail.com>
2024-04-26 09:24:34 +0200

    fix

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,7 +6,7 @@ test.describe('DOB field', () => {
         await page.goto('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_input_type_date');
         await page.click('#accept-choices')
         await page.waitForSelector('#iframeResult');
-        await page.frameLocator('#iframeResult').locator('[type=date]').fill('01/01/1990');
+        await page.frameLocator('#iframeResult').locator('[type=date]').fill('01011990');
         await page.frameLocator('#iframeResult').locator('input[type="submit"]').click();
         await expect(page.frameLocator('#iframeResult').locator('.w3-container.w3-large.w3-border')).toBeVisible();
         await expect(page.frameLocator('#iframeResult').locator('.w3-container.w3-large.w3-border')).toContainText('birthday=1990-01-01 ');
